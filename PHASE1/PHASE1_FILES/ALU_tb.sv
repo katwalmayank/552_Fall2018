1c05a05d002f9081d09682daffa2953124d2bb2d
kangalsucuk <gcpilli@wisc.edu>
2018-10-12 17:11:12 -0500

    Added the RED Test to tb
    Red test is added to ALU tb

--- PHASE1/PHASE1_FILES/ALU_tb.sv
+++ PHASE1/PHASE1_FILES/ALU_tb.sv
@@ -1,14 +1,14 @@
 module ALU_tb();
 
 logic signed [15:0] ALU_Out, ALU_In1, ALU_In2;
-logic [2:0] Opcode, flags;
+logic [2:0] Opcode, Flags;
 logic Error;
 
 logic [16:0] count;
 
-logic signed[15:0] sum_check, sub_check;
+logic signed[15:0] sum_check, sub_check, red_final;
 
-
+logic [7:0] red1, red2, red_sum;
 
 
 assign sum_check = (ALU_In1 > 0 && ALU_In2 > 0 && ALU_Out < 0) ? 16'h7FFF :
@@ -18,8 +18,13 @@ assign sum_check = (ALU_In1 > 0 && ALU_In2 > 0 && ALU_Out < 0) ? 16'h7FFF :
 assign sub_check = (ALU_In1 < 0 && ALU_In2 > 0 && ALU_Out > 0) ? 16'h8000 :
 				   (ALU_In1 > 0 && ALU_In2 < 0 && ALU_Out < 0) ? 16'h7FFF :
 														 ALU_In1 - ALU_In2;
+														 
+assign red1 = ALU_In1[7:0] + ALU_In2[7:0];
+assign red2 = ALU_In1[15:8] + ALU_In2[15:8];
+assign red_sum = red1 + red2;
+assign red_final = {{8{red_sum[7]}}, red_sum[7:0]};
 
-ALU iDUT(.ALU_Out(ALU_Out), .Error(Error), .ALU_In1(ALU_In1), .ALU_In2(ALU_In2), .Opcode(Opcode), .flags(flags));
+ALU iDUT(.ALU_Out(ALU_Out), .Error(Error), .ALU_In1(ALU_In1), .ALU_In2(ALU_In2), .Opcode(Opcode), .Flags(Flags));
 
 initial begin
 
@@ -40,16 +45,16 @@ initial begin
 		
 		#10;
 		if(ALU_Out != sum_check)begin
-			$display("Was expecting 0x%h got 0x%h", sum_check, ALU_Out);
+			$display("For Add was expecting 0x%h got 0x%h", sum_check, ALU_Out);
 			$stop;
 		end
 		
-		if(ALU_Out == 0 && ~flags[2])begin
+		if(ALU_Out == 0 && ~Flags[2])begin
 			$display("Add did not set zero flag ALU_Out = 0x%h", ALU_Out);
 			$stop;
 		end
 		
-		if(ALU_Out[15] && ~flags[0])begin
+		if(ALU_Out[15] && ~Flags[0])begin
 			$display("Add did not set negative flag ALU_Out = 0x%h", ALU_Out);
 			$stop;
 		end
@@ -66,7 +71,7 @@ initial begin
 	
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-//							Opcode = 1 Test
+//					Opcode = 1 Test For Adding ALU_In1 and ALU_In2
 //
 ///////////////////////////////////////////////////////////////////////////////////////////
 	Opcode = 1;
@@ -76,16 +81,16 @@ initial begin
 		
 		#10;
 		if(ALU_Out != sub_check)begin
-			$display("Was expecting 0x%h got 0x%h", sub_check, ALU_Out);
+			$display("For Sub was expecting 0x%h got 0x%h", sub_check, ALU_Out);
 			$stop;
 		end
 		
-		if(ALU_Out == 0 && ~flags[2])begin
+		if(ALU_Out == 0 && ~Flags[2])begin
 			$display("Sub did not set zero flag ALU_Out = 0x%h", ALU_Out);
 			$stop;
 		end
 		
-		if(ALU_Out[15] && ~flags[0])begin
+		if(ALU_Out[15] && ~Flags[0])begin
 			$display("Sub did not set negative flag ALU_Out = 0x%h", ALU_Out);
 			$stop;
 		end
@@ -102,7 +107,7 @@ initial begin
 	
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-//							Opcode = 2 Test
+//					Opcode = 2 Test For Subtracting ALU_In1 and ALU_In2
 //
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -113,7 +118,7 @@ initial begin
 		
 		#10;
 		if(ALU_Out != (ALU_In1 ^ ALU_In2))begin
-			$display("Was expecting 0x%h got 0x%h", (ALU_In1 ^ ALU_In2), ALU_Out);
+			$display("For XOR was as expecting 0x%h got 0x%h", (ALU_In1 ^ ALU_In2), ALU_Out);
 			$stop;
 		end
 		
@@ -123,7 +128,7 @@ initial begin
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-//							Opcode = 3 Test
+//					Opcode = 3 Test For XOR ALU_In1 and ALU_In2
 //
 ///////////////////////////////////////////////////////////////////////////////////////////
 	Opcode = 3;
@@ -132,8 +137,9 @@ initial begin
 		ALU_In2 = $random;
 		
 		#10;
-		if(ALU_Out != (ALU_In1 ^ ALU_In2))begin
-			$display("Was expecting 0x%h got 0x%h", (ALU_In1 ^ ALU_In2), ALU_Out);
+		if(red_final != ALU_Out)begin
+			$display("ALU_In1 0x%h ALU_In2 0x%h", ALU_In1, ALU_In2);
+			$display("RED was expecting 0x%h got 0x%h", red_final, ALU_Out);
 			$stop;
 		end
 		
