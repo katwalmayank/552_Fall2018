98e8920371ba05b62d208fea163c460509f57f77
Thomas Mandel <thomasjmandel@hotmail.com>
2018-10-13 12:01:35 -0500

    CPU.sv and Control.sv updated to correctly handle all opcodes that aren't related to the PC

---
+++ PHASE1/PHASE1_FILES/Control.sv
@@ -0,0 +1,37 @@
+module Control(Opcode, RegDst, Branch, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, Mem, Modify);
+
+input [3:0] Opcode;
+output [2:0] ALUOp;
+output RegDst, Branch, MemtoReg, MemWrite, ALUSrc, RegWrite, Mem, Modify;
+
+// Unused at the moment, maybe we replace Mem with this at some point?
+//assign RegDst = 
+//assign Branch = 
+
+// LW
+assign MemtoReg = (Opcode == 4'b1000);
+
+// SW
+assign MemWrite = (Opcode == 4'b1001);
+
+// ALU gets value from reg2 (default) or from offset/imm?
+// LW, SW
+assign ALUSrc = (Opcode == 4'b1000 | Opcode == 4'b1001);
+
+// This should be set to change rt from the instruction[3:0] to instruction[11:8]
+// Ex. LW, SW
+assign Mem = (Opcode == 4'b1000 | Opcode == 4'b1001);
+
+// The four bit opcode we'll send to the ALU
+// ADD for SW and LW (to compute mem location)
+// Otherwise use last three bits of Opcode
+assign ALUOp = (Mem) ? 3'b000 : Opcode[2:0];
+
+// LW, LLB, LHB, any Compute Op
+assign RegWrite = (Opcode == 4'b1000 | Opcode == 4'b1010 | Opcode == 4'b1011 | ~Opcode[3]);
+
+// This should be set to read from rd instead of rt
+// Ex. LLB, LHB
+assign Modify = (Opcode == 4'b1010 | Opcode == 4'b1011);
+
+endmodule;
--- PHASE1/PHASE1_FILES/cpu.sv
+++ PHASE1/PHASE1_FILES/cpu.sv
@@ -7,13 +7,23 @@ output [15:0] pc;
 
 wire [15:0] instruction, pc_in, pc_out, alu_out, alu_in1, alu_in2, inst_addr, data_addr, data_out, data_in, data_w;
 wire [3:0] opcode, rt, rs, mem_offset, reg1, reg2, reg1_out, reg2_out, dst_reg, dst_data;
-wire [2:0] alu_op;
-wire reg_w;
+wire [2:0] alu_op, ALUOp;
+wire reg_w, RegDst, Branch, MemtoReg, MemWrite, ALUSrc, RegWrite, Mem, Modify;
 
 // ********** INSTRUCTIONS CURRENTLY WIRED UP **********
 // Opcode	Instr
+// 0000		ADD
+// 0001		SUB
+// 0010		XOR
+// 0011		RED
+// 0100		SLL
+// 0101		SRA
+// 0110		ROR
+// 0111		PADDSB
 // 1000		LW
 // 1001		SW
+// 1010		LLB
+// 1011		LHB
 
 // PC Control
 PC_control PC(.opcode(), .data_in(), .C(), .I(), .F(), .PC_in(pc_in), .PC_out(pc_out));
@@ -26,35 +36,40 @@ memory1c InstMem(.data_out(instruction), .data_in(16'bx), .addr(inst_addr), .ena
 
 assign inst_addr = pc_out;
 assign opcode = instruction[15:12];
-assign rt = instruction[11:8];
+assign rd = instruction[11:8];
 assign rs = instruction[7:4];
+assign rt = (Mem) ? instruction[11:8] : instruction[3:0];
 assign mem_offset = instruction[3:0];
+assign imm = instruction[7:0];
+
+// Control Unit
+Control Contr(.Opcode(opcode), .RegDst(RegDst), .Branch(Branch), .MemtoReg(MemtoReg), .ALUOp(ALUOp), .MemWrite(MemWrite),
+		.ALUSrc(ALUSrc), .RegWrite(RegWrite), .Mem(Mem), .Modyify(Modify));
 
 // Data Memory
 memory1c DataMem(.data_out(data_out), .data_in(data_in), .addr(data_addr), .enable(1'b1), .wr(data_w), .clk(clk), .rst(~rst_n)); 
 
-assign data_addr = (opcode == 4'b1000 | opcode == 4'b1001) ? alu_out : 16'bx;
-assign data_in = (opcode == 4'b1001) ? reg2_out : 16'bx;
-assign data_w = (opcode == 4'b1001);
+assign data_addr = alu_out;
+assign data_in = reg2_out;
+assign data_w = MemWrite;
 
 // Registers
 RegisterFile Registers(.clk(clk), .rst(~rst_n), .SrcReg1(reg1), .SrcReg2(reg2), .DstReg(dst_reg), .WriteReg(reg_w), .DstData(dst_data), .SrcData1(reg1_out), .SrcData2(reg2_out));
 
-assign reg1 = (opcode == 4'b1000 | opcode == 4'b1001) ? rs : 4'bx;
-assign reg2 = (opcode == 4'b1001) ? rt : 4'bx;
-assign dst_reg = (opcode == 4'b1000) ? rt : 4'bx;
-assign dst_data = (opcode == 4'b1000) ? data_out : 16'bx;
-assign reg_w = (opcode == 4'b1000);
+assign reg1 = rs;
+assign reg2 = (Modify) ? rd : rt;
+assign dst_reg = (Mem) ? rt : rd;
+assign dst_data = (MemtoReg) ? data_out :
+		  (~Modify) ? alu_out :
+		  (opcode == 4'b1010) ? ((reg2_out & 16'hFF00) | {8'b0, imm}) : 
+		  ((reg2_out & 16'h00FF) | {imm, 8'b0});
+assign reg_w = RegWrite;
 
 // ALU
 ALU Alu(.ALU_Out(alu_out), .Error(), .ALU_In1(alu_in1), .ALU_In2(alu_in2), .Opcode(alu_op), .Flags());
 
-assign alu_op = (opcode == 4'b1000 | opcode == 4'b1001) ? 3'b000 : 4'bx;
-assign alu_in1 = (opcode == 4'b1000 | opcode == 4'b1001) ? reg1_out : 16'bx;
-assign alu_in2 = (opcode == 4'b1000 | opcode == 4'b1001) ? mem_offset : 16'bx;
-
-
-
-
+assign alu_op = ALUOp;
+assign alu_in1 = reg1_out;
+assign alu_in2 = ~ALUSrc ? reg2_out : {{12{mem_offset[3]}},  mem_offset} << 1;
 
 endmodule
