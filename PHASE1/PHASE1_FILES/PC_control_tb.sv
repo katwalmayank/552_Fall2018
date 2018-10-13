645be2c2227e4dae33a5449ccf99d908a874201e
katwalmayank <Nightmarewr360@gmail.com>
2018-10-13 12:38:23 -0500

    dff 16 bit added

--- PHASE1/PHASE1_FILES/PC_control_tb.sv
+++ PHASE1/PHASE1_FILES/PC_control_tb.sv
@@ -17,7 +17,7 @@ initial begin
 						I = 9'b000000011;
 						data_in = 16'b0001000100011111;
 						PC_in = 16'b0000000000000001;
-						#5 $display("C: %b  F: %b I: %b  PC_in: %b  PC_out %b", C,F,I,PC_in,PC_out);
+						#5 $display("C: %b  F: %b opcode: %b I: %b  PC_in: %b  PC_out %b", C,F,opcode, I,PC_in,PC_out);
 				end
 			end 
 		end
